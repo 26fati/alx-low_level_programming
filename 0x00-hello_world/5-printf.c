56a89cf3d4ccd9461f87615ecf2fc46fec563c9c
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-03-16 21:42:54 +0100

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: A C program that prints with protf function.
+ *
+ * Return: return 0 if exited successfuly.
+ */
+
+int main(void)
+{
+	printf("with prper grammar, but the outcome is a piece of art,");
+	return (0);
+}
