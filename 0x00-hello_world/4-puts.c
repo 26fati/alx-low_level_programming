0dcea3f3b67a61b1b7f0e8a2bd7351d12e09e263
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-03-16 17:48:17 +0100

    a C program that prints a string using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/*
+ * main - prints exactly "Programming is like building a multilingual puzzle 
+ * and return 0 is exited properly. 
+ */
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
