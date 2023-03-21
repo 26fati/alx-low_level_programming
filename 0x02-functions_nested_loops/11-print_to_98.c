6bc95ea1c6c99bfc8b5a68faa1ecdc80f7f68d63
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-03-21 14:58:30 +0100

    0

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,15 +16,19 @@ void print_to_98(int n)
 	else if (n > 98)
 	{
 		while (n != 98)
+		{
 			printf("%i, ", n);
 			n--;
+		}
 
 	}
 	else
 	{
 		while (n != 98)
+		{
 			printf("%i, ", n);
 			n++;
+		}
 	}
 	_putchar('\n');
 }
