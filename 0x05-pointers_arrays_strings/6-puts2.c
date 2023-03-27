283c06554f9c5ccc5adeeafff7e2b282c0309731
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-03-27 19:13:23 +0200

    1

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,6 +12,11 @@ void puts2(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i = i + 2)
-		_putchar(str[i]);
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
 	_putchar('\n');
+	}
 }
