9ba554bd6408d110a18e7ea3bb6579cf4c8761fa
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-03-29 23:34:45 +0200

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,9 +17,9 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-	while (*(src + i) != '\0' && j < n))
+	while (*(src + i) != '\0' && j < n)
 	{
-		*(dest + i) = *(src + j)
+		*(dest + i) = *(src + j);
 		src++;
 		dest++;
 	}
