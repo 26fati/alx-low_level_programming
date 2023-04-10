6b5aac0fd7be22042fc3dedbf1fce79b28c759fc
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-04-10 12:41:33 +0200

    1

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<stdlib.h>
+/**
+ * main -  a program that adds positive numbers.
+ *
+ * @argc: the number of commands.
+ * @argv: the comaands line arguments.
+ *
+ * Return: return 0 (success) otherwise 1.
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	char *c;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+
+		result = result + atoi(argv[argc]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
