ed85feb79de2e1aae3ee358c191724a19302f97e
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-04-11 14:34:11 +0200

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,15 +2,23 @@
 #include <stdlib.h>
 char *_strdup(char *str)
 {
-	int i = 0;
-	char *arr = malloc(sizeof(str));
+	int i = 0, size = 0;
+	char *arr;
 
-	if (str == 0)
-		return (0);
+	if (str == NULL)
+		return (NULL);
+	for (; str* != '\0'; size++)
+	;
+	arr = malloc(size * sizeof(str) + 1);
+	if (arr == 0)
+		return (NULL);
+	else
+	{
 	while (*str)
 	{
 		arr[i] = str[i];
 		i++;
+	}
 	}
 	return (arr);
 }
