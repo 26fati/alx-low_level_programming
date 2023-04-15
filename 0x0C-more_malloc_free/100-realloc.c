3958e8ecb96c9f9a2439193f50e2e92d1ad1c2f9
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-04-15 23:46:55 +0200

    1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,12 +19,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		p = malloc(new_size);
 		return (p);
 	}
-	else if (new_size == 0)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else if (old_size == new_size)
+	if (old_size == new_size)
 		return (ptr);
 
 	p = malloc(new_size);
