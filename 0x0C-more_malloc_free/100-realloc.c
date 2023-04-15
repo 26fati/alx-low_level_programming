57b04243c3b05ea7b93c97db3af6b3835355d10f
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-04-15 23:09:24 +0200

    1

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - Main Entry
+ * @ptr: input
+ * @old_size: input
+ * @new_size: input
+ * Return: 0
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	char *oldptr = ptr;
+	unsigned int i, max = new_size;
+
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (old_size == new_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		max = new_size;
+	for (i = 0; i < max; i++)
+		p[i] = oldptr[i];
+	free(ptr);
+	return (p);
+}
