c8c14b1d4c879c17cd9644d19bae85e1fba1da10
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-04-17 11:59:26 +0200

    1

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef function_like_macro.h
-#define function_like_macro.h
+#ifndef function_like_macro_h
+#define function_like_macro_h
 
-#define ABS(X) ((x < 0) ? (x * (-1)) : x)
+#define ABS(X) ((x) < 0 ? (x) * -(1) : (x))
 
 #endif
