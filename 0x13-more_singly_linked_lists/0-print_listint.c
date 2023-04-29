0416f85163b6378c4fca728578c147559327b8fe
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-04-29 22:00:45 +0200

    1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,12 +13,17 @@ size_t print_listint(const listint_t *h)
 	size_t count = 0;
 
 	if (h == NULL)
+	{
 		return (0);
+	}
+	else
+	{
 	while (h)
 	{
 		count++;
 		printf("%d\n", h->n);
 		h = h->next;
+	}
 	}
 	return (count);
 
