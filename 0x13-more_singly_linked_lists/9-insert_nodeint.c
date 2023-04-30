a03770c85319347237361c0cb3d40b0562cb9a47
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-04-30 14:31:20 +0200

    1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		for (node = 0; node < idx; node++)
 		{
-			if (tmp == NULL)
+			if (tmp->next == NULL)
 				return (NULL);
 			tmp = tmp->next;
 		}
