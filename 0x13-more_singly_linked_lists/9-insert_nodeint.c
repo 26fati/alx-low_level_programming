916f4ffd489b2251489d2d2c40b94dfc0bbbef34
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-04-30 13:32:57 +0200

    1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,8 +36,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		tmp = tmp->next;
 	}
-	ptr->next = tmp;
-	tmp = ptr;
+	ptr->next = tmp->next;
+	tmp->next = ptr;
 	}
 	return (ptr);
 }
