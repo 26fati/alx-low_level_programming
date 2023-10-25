3bef7d06e0fa3ac48872ae39f361e2be259af503
26fati <fatibeautiful1@gmail.com>
2023-10-25 11:01:55 +0100

    tasks

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    ''' a function that returns the perimeter
+        of the island described in grid
+    '''
+    total = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                total += 4
+                if x > 0:
+                    total = total - grid[x-1][y] * 2
+                if y > 0:
+                    total = total - grid[x][y-1] * 2
+    return total
